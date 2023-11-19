89b17e24ba38d2778c1b8faf8ae0a8d52875f3fb
amayou21 <amaguaryoussefpay10@gmail.com>
2023-11-19 14:40:05 +0100

    first c# tuto

---
+++ DEMO/Personne.cs
@@ -0,0 +1,10 @@
+namespace DEMO
+{
+    class Personne
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("hello other namespace");
+        }
+    }
+}
---
+++ DEMO/Program.cs
@@ -0,0 +1,25 @@
+﻿// using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
+using DEMO;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static object getMax(ref int num1, ref int num2)
+        {
+            num1 = 18;
+            num2 = 78;
+            return num1 + " " + num2;
+        }
+        private static void Main(string[] args)
+        {
+            Personne.SayHello();
+            int num1 = 12;
+            int num2 = 16;
+            Console.WriteLine(getMax(ref num1, ref num2));
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+        }
+    }
+}
